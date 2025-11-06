7c82ffec157f403357b871cc37ea0bace74c4400
RubenUex <rvazquezva@alumnos.unex.es>
2025-11-06 17:57:29 +0100

    Creadas pruebas de jUnit y Mockito

--- src/test/java/es/upm/grise/profundizacion/cruiseController/CruiseControlTest.java
+++ src/test/java/es/upm/grise/profundizacion/cruiseController/CruiseControlTest.java
@@ -1,5 +1,50 @@
-package es.upm.grise.profunduzacion.cruiseController;
+package es.upm.grise.profundizacion.cruiseController;
+
+import es.upm.grise.profundizacion.cruiseControl.CruiseControl;
+import es.upm.grise.profundizacion.cruiseControl.Speedometer;
+import es.upm.grise.profundizacion.exceptions.IncorrectSpeedSetException;
+import es.upm.grise.profundizacion.exceptions.SpeedSetAboveSpeedLimitException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
 
 class CruiseControlTest {
-	
+
+	@Test
+	void speedSetTieneQueSerNuloAlInicializar() {
+		Speedometer speedometer = mock(Speedometer.class);
+		CruiseControl cruiseControl = new CruiseControl(speedometer);
+		assertNull(cruiseControl.getSpeedSet());
+	}
+
+	@Test
+	void speedLimitTieneQueSerNuloAlInicializar() {
+		Speedometer speedometer = mock(Speedometer.class);
+		CruiseControl cruiseControl = new CruiseControl(speedometer);
+		assertNull(cruiseControl.getSpeedLimit());
+	}
+
+	@Test
+	void speedSetNoPuedeSerCero() {
+		Speedometer speedometer = mock(Speedometer.class);
+		CruiseControl cruiseControl = new CruiseControl(speedometer);
+		assertThrows(IncorrectSpeedSetException.class, () -> cruiseControl.setSpeedSet(0));
+	}
+
+	@Test
+	void speedSetNoPuedeSerNegativo() {
+		Speedometer speedometer = mock(Speedometer.class);
+		CruiseControl cruiseControl = new CruiseControl(speedometer);
+		assertThrows(IncorrectSpeedSetException.class, () -> cruiseControl.setSpeedSet(-1));
+	}
+
+	@Test
+	void speedSetNoPuedeSuperarSpeedLimit() {
+		Speedometer speedometer = mock(Speedometer.class);
+		CruiseControl cruiseControl = new CruiseControl(speedometer);
+		cruiseControl.setSpeedLimit(120);
+		assertThrows(SpeedSetAboveSpeedLimitException.class, () -> cruiseControl.setSpeedSet(121));
+	}
 }
